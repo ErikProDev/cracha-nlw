0b6ac15dc15d6263ce3275e226d3f3849fbfdec9
Erik Martins <68782855+GodWolf277@users.noreply.github.com>
2021-10-22 18:01:16 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+/*       // variáveis let e const
+      let accept = true
+      let user = {
+        name: 'Erik Martins',
+        phone: 123456789
+      }
+      //alert(user.name) */
+/*       function showMeSomething() {
+        alert(links.instagram)
+      } */
+const linksSocialMedia = {
+  github: 'GodWolf277',
+  youtube: 'GamesEduUu',
+  facebook: 'erikmartins',
+  instagram: 'erikmartins50',
+  linkedin: 'in/erikmartinsbr/'
+}
+//showMeSomething(link.github)
+function changeSocialMediaLinks() {
+  /*         for (let i = 0; i <= 10; i++) {
+          alert(i)
+        } */
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    /* li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}` */
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    //alert(li.children[0].href)
+  }
+  //document.getElementById('userName').textContent = 'Lucas'
+  //userName.textContent = 'Nara Martins'
+}
+// i = i + 1
+// i++
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userProfile.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
+
+/* //ARROW FUNCTIONS
+function nomeDaFuncao(args){
+  //code
+}
+//função anônima
+args => {
+
+}
+
+nomeDaFuncao() */
